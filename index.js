1054e5f75bdd9e89c0576b9b2aa0a8e298a4600e
Usman Akram <usman.akram1996@outlook.com>
2018-03-23 19:09:20 +0500

    Assignment No 1

--- index.js
+++ index.js
@@ -4,31 +4,23 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-// app.get('/dishes/:dishId', (req,res,next) => {
-//     res.end('Will send details of the dish: ' + 
-//         req.params.dishId + ' to you!');
-// });
 
-// app.post('/dishes/:dishId', (req,res,next) => {
-//     res.end('PUT operation not supported on /dishes/' +
-//         req.params.dishId );
-// });
-
-// app.put('/dishes/:dishId', (req,res,next) => {
-//     res.write('Updating the dish: ' + req.params.dishId + '\n');
-//     res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description );
-// });
-
-// app.delete('/dishes/:dishId', (req,res,next) => {
-//     res.end('Deleting dish: ' + req.params.dishId);
-// });
 app.use('/dishes', dishRouter);
+app.use( '/dishes/:dishId' , dishRouter);
+app.use('/promotions', promoRouter);
+app.use( '/promotions/:promoId' , promoRouter);
+app.use('/leaders', leaderRouter);
+app.use( '/leaders/:leaderId' , leaderRouter);
+
 app.use(express.static(__dirname + '/public'));
 
 app.use((req,res,next) => {
